44effce10c827889c43220afb1445edf01ba3744
tachengP <69946673+tachengP@users.noreply.github.com>
2022-03-26 20:12:01 +0800

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -50,8 +50,8 @@ module.exports = {
         desc: 'A simple and beautiful vuepress Blog & Doc theme.',
         avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
         link: 'https://vuepress-theme-reco.recoluan.com'
-      },*/
-    ],
+      },
+    ],*/
     logo: '/logo.png',
     // 搜索设置
     search: true,
